ac77ad9da8289dc52ef3762912a57a34aac98146
AySqirl <aymericjacob@outlook.com>
2022-01-18 16:26:46 +0100

    add start createChannel

--- src/components/Channel.vue
+++ src/components/Channel.vue
@@ -5,16 +5,37 @@ const props = defineProps({
     channelList: Array
 });
 
-const emits = defineEmits(['change']);
+const emits = defineEmits({
+    "update:selectedChannel": Number
+});
 
 const selectedChannel = ref('');
-
+const token = ref('eyJhbGciOiJIUzI1NiJ9.eyJhZG1pbiI6ImZhbHNlIiwiaWF0IjoiMTY0MjUxMjk2NCIsInN1YiI6ImFqYWNvYiJ9.E-7iJLetcp_-Esp3krrn59YMAZsy4bFyV-tJD3rAiO8');
 
 
 function setSelectedChannel(newId){
     selectedChannel.value = newId;
 }
 
+async function createChannel(){
+    //voir pour ajouter un popup pourrenseigner les champs
+    // name
+    // img
+    json = null;
+    const options = {
+      method: "PUT",
+      body: json,
+      headers: {
+        Authorization: 'Bearer '+ token.value 
+      }
+    };
+    const response = await fetch('https://edu.tardigrade.land/msg/protected/channel', options);
+    if (response.status == 200) {
+        response.json().then(data => {
+            selectedChannel.value = data.id;
+        });
+    }
+}
 
 
 </script>
@@ -27,7 +48,7 @@ function setSelectedChannel(newId){
         </div>
     </section>
     <section>
-        <div id="createChannel">
+        <div id="createChannel" @click="createChannel">
             <img> <!-- mettre un plus -->
             <p>Créer un channel</p>
         </div>
