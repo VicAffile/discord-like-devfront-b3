2bb4ebb52b8def92141d94cb819d234732b3db38
AySqirl <aymericjacob@outlook.com>
2022-01-18 15:29:24 +0100

    add channel comp

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Login from './components/Login.vue'
+import Login from './components/Login.vue';
+import Channel from './components/Channel.vue';
+
+import { ref } from 'vue';
+
+const channelList = ref([{name: "bonjour", img: null, id:1},{name: "au revoir", img: null, id:2}]);
+//const channelList = ref([]);
+
+async function getChannel() {
+  const options = {
+      method: "GET",
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhZG1pbiI6ImZhbHNlIiwiaWF0IjoiMTY0MjUxMjk2NCIsInN1YiI6ImFqYWNvYiJ9.E-7iJLetcp_-Esp3krrn59YMAZsy4bFyV-tJD3rAiO8' 
+      }
+  };
+  const response = await fetch('https://edu.tardigrade.land/msg/protected/user/channels', options);
+  if (response.status == 200) {
+      response.json().then(data => {
+          console.log(data)
+          channelList.value = data;
+      });
+  }
+}
+
+//getChannel();
 </script>
 
 <template>
   <Login />
+  <Channel :channelList="channelList" />
 </template>
 
 <style>
@@ -18,3 +43,6 @@ import Login from './components/Login.vue'
   margin-top: 60px;
 }
 </style>
+
+
+<!-- eyJhbGciOiJIUzI1NiJ9.eyJhZG1pbiI6ImZhbHNlIiwiaWF0I…vYiJ9.femrt8gdtxBkzz62zzxY-8k1mppaf9NvwuR262AUvQg -->
---
+++ src/components/Channel.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    channelList: Array
+});
+
+const emits = defineEmits(['change']);
+
+const selectedChannel = ref('');
+
+
+
+function setSelectedChannel(newId){
+    selectedChannel.value = newId;
+}
+
+
+
+</script>
+
+<template>
+    <section v-for="channel in channelList">
+        <div :id="channel.id" @click="setSelectedChannel(channel.id)">
+            <img :src="channel.img">
+            <p>{{ channel.name }}</p>
+        </div>
+    </section>
+    <section>
+        <div id="createChannel">
+            <img> <!-- mettre un plus -->
+            <p>Créer un channel</p>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+</style>
